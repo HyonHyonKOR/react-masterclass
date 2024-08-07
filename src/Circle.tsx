cabd2c525bb266edd7a963536c70d91aa759fe6a
HyonHyon <khm3626@gmail.com>
2024-08-07 21:18:23 +0900

    feat: typescript practive with React props2

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,29 +2,25 @@ import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "default" }: CircleProps) {
+  return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
 }
 
 export default Circle;
-
-interface PlayerShape {
-  name: string;
-  age: number;
-}
-const sayHello = (playerObj: PlayerShape) =>
-  `Hello ${playerObj.name} I'm ${playerObj.age} years old`;
-
-sayHello({ name: "Jason", age: 15 });
