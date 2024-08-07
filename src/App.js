7a641b5e924090d573363af7091a01b0bab932ce
HyonHyonKOR <khm3626@gmail.com>
2024-08-07 14:28:57 +0900

    feat: adapting and extending(Styledcomponents)

--- src/App.js
+++ src/App.js
@@ -5,28 +5,20 @@ function App() {
     display: flex;
   `;
 
-  const BoxOne = styled.div`
-    background-color: teal;
+  const Box = styled.div`
+    background-color: ${(props) => props.bgColor};
     width: 100px;
     height: 100px;
   `;
 
-  const BoxTwo = styled.div`
-    background-color: tomato;
-    width: 100px;
-    height: 100px;
-  `;
-
-  const Text = styled.span`
-    color: white;
+  const Circle = styled(Box)`
+    border-radius: 50%;
   `;
 
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo></BoxTwo>
+      <Circle bgColor="tomato"></Circle>
+      <Box bgColor="whitesmoke"></Box>
     </Father>
   );
 }
