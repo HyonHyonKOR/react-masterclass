368344badd093d8988995381e867bb1229a3c9fe
HyonHyonKOR <khm3626@gmail.com>
2024-08-07 16:22:38 +0900

    feat: theme(Styledcomponents)

--- src/App.js
+++ src/App.js
@@ -1,54 +1,22 @@
 import styled, { keyframes } from "styled-components";
 
 function App() {
+  const Title = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
+
   const Wrapper = styled.div`
     display: flex;
-  `;
-
-  const rotate = keyframes`
-0%{
-  transform: rotate(0deg);
-  border-radius: 0px;
-}
-50%{
-  transform: rotate(180deg);
-  border-radius: 100px;
-}
-100%{
-  transform: rotate(360deg);
-  border-radius: 0px;
-}
-`;
-
-  const Emoji = styled.span`
-    font-size: 36px;
-  `;
-
-  const Box = styled.div`
-    height: 200px;
-    width: 200px;
-    background-color: tomato;
-    display: flex;
+    height: 100vh;
+    width: 100vw;
     justify-content: center;
     align-items: center;
-    animation: ${rotate} 1s linear infinite;
-    ${Emoji} {
-      font-size: 36px;
-      transition: all 1s ease-in;
-      &:hover {
-        font-size: 80px;
-      }
-      &:active {
-        opacity: 0;
-      }
-    }
+    background-color: ${(props) => props.theme.backgroundColor};
   `;
 
   return (
     <Wrapper as="header">
-      <Box>
-        <Emoji as="a">😉</Emoji>
-      </Box>
+      <Title>Dark mode</Title>
     </Wrapper>
   );
 }
