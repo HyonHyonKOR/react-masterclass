0fe7530e1ccbebcc44e14e89229ad695fce9a1f3
HyonHyonKOR <khm3626@gmail.com>
2024-08-07 14:47:08 +0900

    feat: as and attrs (Styledcomponents)

--- src/App.js
+++ src/App.js
@@ -5,20 +5,17 @@ function App() {
     display: flex;
   `;
 
-  const Box = styled.div`
-    background-color: ${(props) => props.bgColor};
-    width: 100px;
-    height: 100px;
-  `;
-
-  const Circle = styled(Box)`
-    border-radius: 50%;
+  const Input = styled.input.attrs({ required: true })`
+    background-color: tomato;
   `;
 
   return (
-    <Father>
-      <Circle bgColor="tomato"></Circle>
-      <Box bgColor="whitesmoke"></Box>
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
