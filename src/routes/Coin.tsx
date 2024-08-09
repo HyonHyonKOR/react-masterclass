807a3645486fab28ae932ddf611c17398feb43e5
HyonHyonKOR <khm3626@gmail.com>
2024-08-09 15:38:30 +0900

    feat:make detail page

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -25,6 +25,28 @@ const Loader = styled.div`
   display: block;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 interface RouteState {
   state: {
     name: string;
@@ -56,20 +78,28 @@ const Coin = () => {
   const { coinId } = useParams();
   const [loading, setLoading] = useState(true);
   const { state } = useLocation() as RouteState;
-  const [coinInfo, setCoinInfo] = useState<InfoData[]>();
+  const [coinInfo, setCoinInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
   useEffect(() => {
     (async () => {
-      const coinData = await (
+      const coinDataList = await (
         await fetch(`https://api.coinpaprika.com/v1/coins`)
       ).json();
-      const priceData = await (
+      const priceDataList = await (
         await fetch(
           `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
         )
       ).json();
+      const coinData: InfoData = coinDataList
+        .slice(0, 100)
+        .find((item: InfoData) => (item.id = `${coinId}`));
       setCoinInfo(coinData);
+
+      console.log(priceDataList);
+      const priceData: PriceData = priceDataList[0];
+      console.log(priceData);
       setPriceInfo(priceData);
+      setLoading(false);
     })();
   }, []);
   return (
@@ -77,7 +107,45 @@ const Coin = () => {
       <Header>
         <Title>{state?.name || "Loading.."}</Title>
       </Header>
-      {loading ? <Loader>"Loading..."</Loader> : null}
+      {loading ? (
+        <Loader>"Loading..."</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{coinInfo?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>{coinInfo?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Type:</span>
+              <span>{coinInfo?.type}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>
+            {state?.name} is a {coinInfo?.type}.Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Impedit fugit maxime, culpa unde sint
+            delectus quo enim veniam magnam amet commodi dicta sunt cum
+            excepturi dolore, est nam, ab nulla? Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Atque, unde beatae sed assumenda autem
+            ratione omnis. Voluptatum, eos quaerat veniam consectetur aliquam
+            amet aliquid nesciunt, placeat nobis voluptates inventore autem!
+          </Description>
+          <Overview>
+            <OverviewItem>
+              <span>Max Price:</span>
+              <span>{priceInfo?.high}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Low Price:</span>
+              <span>{priceInfo?.low}</span>
+            </OverviewItem>
+          </Overview>
+        </>
+      )}
     </Container>
   );
 };
