51c35949070cbc42fd1d248427f9ec88ccaa6285
HyonHyonKOR <khm3626@gmail.com>
2024-08-09 14:32:34 +0900

    feat: type practice

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
-  padding: 0px 20px;
+  padding: 20px 0;
   max-width: 480px;
   margin: 0 auto;
 `;
@@ -31,11 +31,47 @@ interface RouteState {
   };
 }
 
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+interface PriceData {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
 const Coin = () => {
   const { coinId } = useParams();
   const [loading, setLoading] = useState(true);
   const { state } = useLocation() as RouteState;
-
+  const [coinInfo, setCoinInfo] = useState<InfoData[]>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+  useEffect(() => {
+    (async () => {
+      const coinData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins`)
+      ).json();
+      const priceData = await (
+        await fetch(
+          `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+        )
+      ).json();
+      setCoinInfo(coinData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
-  padding: 0px 20px;
+  padding: 20px 0;
   max-width: 480px;
   margin: 0 auto;
 `;
