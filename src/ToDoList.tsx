bd46c15d6971e4834386f50271664871e8d0c295
HyonHyon <khm3626@gmail.com>
2024-08-13 11:44:12 +0900

    feat:practice custom validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -31,8 +31,14 @@ import { useForm } from "react-hook-form";
 //   );
 // };
 
-interface FormData {
-  [key: string]: string;
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+  extraError?: string;
 }
 
 const ToDoList = () => {
@@ -40,13 +46,23 @@ const ToDoList = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>({
+    setError,
+    setValue,
+  } = useForm<IForm>({
     defaultValues: {
       email: "@naver.com",
     },
   });
-  const onValid = (data: any) => {
-    console.log(data);
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "Password are not the same" },
+        { shouldFocus: true }
+      );
+    }
+
+    //setError("extraError", { message: "Server offline." });
   };
   return (
     <div>
@@ -55,29 +71,59 @@ const ToDoList = () => {
         onSubmit={handleSubmit(onValid)}
       >
         <input
-          {...register("you need to input id", {
-            required: true,
-            minLength: {
-              value: 5,
-              message: "your id is too short",
-            },
-          })}
-        />
-        <input {...register("pw1", { required: true })} />
-        <input {...register("pw2", { required: true })} />
-        <input
           {...register("email", {
-            required: "you need to input email",
+            required: "Email is required",
             pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
               message: "Only naver.com emails allowed",
             },
           })}
           placeholder="Email"
         />
         <span>{errors?.email?.message}</span>
-        <input {...register("mobile", { required: true })} />
+        <input
+          {...register("firstName", {
+            required: "write here",
+            validate: {
+              noNico: async (value) =>
+                value.includes("nico") ? "no nicos allowed" : true,
+              noNick: (value) =>
+                value.includes("nick") ? "no nick allowed" : true,
+            },
+          })}
+          placeholder="First Name"
+        />
+        <span>{errors?.firstName?.message}</span>
+        <input
+          {...register("lastName", {
+            required: "write here",
+          })}
+          placeholder="Last Name"
+        />
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register("username", { required: "write here", minLength: 10 })}
+          placeholder="Username"
+        />
+        <span>{errors?.username?.message}</span>
+        <input
+          {...register("password", { required: "write here", minLength: 5 })}
+          placeholder="Password"
+        />
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("password1", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
+          placeholder="Password1"
+        />
+        <span>{errors?.password1?.message}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
